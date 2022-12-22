5a84dcfab2ca16e7d37847e3253de7bbb961d30f
Christopher <chris.hem10@gmail.com>
2022-12-22 17:34:04 -0600

    Changes for V2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Greetings, World!
+          Greetings, World! I'm from V2!
         </p>
         <a
           className="App-link"
